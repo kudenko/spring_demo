da629594ae710443541257485dd57a15c3549e56
Sergey Kudenko <sergeykudenko@Sergeys-MacBook-Air.local>
2025-03-09 18:04:43 +0100

    java 23 changes

---
+++ src/main/java/ua/goit/spring_demo/SpringDemoApplication.java
@@ -0,0 +1,14 @@
+package ua.goit.spring_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class SpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoApplication.class, args);
+	}
+
+}
